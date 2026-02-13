614ffa72d9b5b00665c57435dfe1c678e9475f58
Amos Amissah <39559758+theonlyamos@users.noreply.github.com>
2026-02-13 16:05:57 +0000

    chore: bump version to 0.4.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
-VERSION = "0.4.3"
+VERSION = "0.4.4"
 
 with open('README.md', 'rt') as file:
     description = file.read()
