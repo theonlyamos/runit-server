5e4f2136b4d0c11f46b0636191ff4eb4a05715b9
Amos Amissah <39559758+theonlyamos@users.noreply.github.com>
2026-02-15 12:11:12 +0000

    bump version to 0.4.6, building new docker image

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
-VERSION = "0.4.5"
+VERSION = "0.4.6"
 
 with open('README.md', 'rt') as file:
     description = file.read()
